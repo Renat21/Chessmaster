111df541be334fce321299ab37161a6a88d14c98
Sergey-hub02 <s.park190802@gmail.com>
2022-07-05 22:00:39 +0300

    Создал структуру проекта и добавил зависимости

---
+++ src/main/java/ru/chess/chessmaster/ChessmasterApplication.java
@@ -0,0 +1,13 @@
+package ru.chess.chessmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessmasterApplication.class, args);
+	}
+
+}
